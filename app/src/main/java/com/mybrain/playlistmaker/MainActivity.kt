56dfc7b95e9157759de64ab9aa54550cf2fec82b
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2025-07-15 23:21:01 +0500

    - фикс верстки

--- app/src/main/java/com/mybrain/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/mybrain/playlistmaker/MainActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
 
--- app/src/main/java/com/mybrain/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/mybrain/playlistmaker/SearchActivity.kt
@@ -1,6 +1,7 @@
 package com.mybrain.playlistmaker
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
@@ -8,6 +9,7 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.search_activity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
