6cebb72a8186d6b03bdde8a56a3c95655cb297f6
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2025-10-15 23:53:47 +0500

    задание по спринту 10

---
+++ app/src/main/java/com/mybrain/playlistmaker/search/SearchTrackVH.kt
@@ -0,0 +1,39 @@
+package com.mybrain.playlistmaker.search
+
+import android.view.LayoutInflater
+import android.view.RoundedCorner
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.mybrain.playlistmaker.R
+import com.mybrain.playlistmaker.models.Track
+
+class SearchTrackVH(
+    parent: ViewGroup,
+): RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.search_item_track, parent, false)) {
+    private val image: ImageView = itemView.findViewById(R.id.ivArtwork)
+    private val name: TextView = itemView.findViewById(R.id.tvTitle)
+    private val author: TextView = itemView.findViewById(R.id.tvAuthor)
+    private val time: TextView = itemView.findViewById(R.id.tvTime)
+
+    fun bind(track: Track) {
+        val radius = itemView.context.resources.getDimensionPixelSize(R.dimen.corner_2)
+
+        Glide.with(itemView)
+            .load(track.artworkUrl100)
+            .transform(CenterCrop(), RoundedCorners(radius))
+            .placeholder(R.drawable.placeholder_track)
+            .error(R.drawable.placeholder_track)
+            .skipMemoryCache(true)
+            .fallback(R.drawable.placeholder_track)
+            .into(image)
+        name.text = track.trackName
+        author.text = track.artistName
+        time.text = track.trackTime
+    }
+}
