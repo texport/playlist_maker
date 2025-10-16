8288776408d6a9f73b92d1b906a7b6037efc62bc
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2025-10-16 14:47:23 +0500

    fix высоты ячейки

--- app/src/main/java/com/mybrain/playlistmaker/data/MockTracks.kt
+++ app/src/main/java/com/mybrain/playlistmaker/data/MockTracks.kt
@@ -33,6 +33,144 @@ object MockTracks {
             artistName = "Guns N' Roses",
             trackTime = "5:03",
             artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
         )
     )
 
